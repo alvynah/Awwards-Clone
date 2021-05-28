87355def3b80a092650e08351a099ce189875178
alvynah <alvynahw@gmail.com>
2021-05-28 18:33:42 +0300

    Initial database migration

--- AwwardsClone/settings.py
+++ AwwardsClone/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'AwwardsClone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awwards',
+        'USER': 'cocoh',
+        'PASSWORD': 'kitkAt10',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
