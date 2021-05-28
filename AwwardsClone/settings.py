f209f162e92bd76c30a41d74ea16008f9ecf9d7d
alvynah <alvynahw@gmail.com>
2021-05-28 20:29:22 +0300

    Fix bug when logging in function

--- AwwardsClone/settings.py
+++ AwwardsClone/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
+LOGIN_REDIRECT_URL = 'welcome'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
