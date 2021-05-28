82ba99d44645350e62ff61db977190e8ad73bbbf
alvynah <alvynahw@gmail.com>
2021-05-28 20:17:31 +0300

    Log out a user

--- AwwardsClone/urls.py
+++ AwwardsClone/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('',include('awwards.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/')),
 ]
